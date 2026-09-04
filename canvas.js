d86f2c859ad76ca01aebb7f73c6cb47bce7318e1
Ankoder-tech <beingankit888@gmail.com>
2026-09-05 02:42:45 +0530

    Added EventListener(mousemove)

--- canvas.js
+++ canvas.js
@@ -3,7 +3,18 @@ canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 var c=canvas.getContext('2d');
 
+var mouse={
+    x:undefined,
+    y:undefined
+}
+var maxradius=40;
+var minradius=5;
 
+window.addEventListener('mousemove',function(event){
+    mouse.x=event.x;
+    mouse.y=event.y;
+
+})
 
 function Circle(x,y,dx,dy,radius) {
 
@@ -31,8 +42,19 @@ function Circle(x,y,dx,dy,radius) {
             this.dy=-this.dy;
         }
 
+
         this.x+=this.dx;
         this.y+=this.dy;
+        if(mouse.x-this.x<50 && mouse.x-this.x>-50 && 
+            mouse.y-this.y<50 && mouse.y-this.y>-50){
+
+            if(this.radius<maxradius){
+                this.radius+=1;
+            }
+        }
+        else if(this.radius>minradius){
+            this.radius-=1;
+        }
         this.draw();
 
     }
@@ -40,12 +62,12 @@ function Circle(x,y,dx,dy,radius) {
 
 
 var circleArray=[];
-for(var i=0;i<100;i++){
+for(var i=0;i<1000;i++){
     var x=Math.random()*(innerWidth - radius*2)+radius;
     var y=Math.random()*(innerHeight - radius*2)+radius;
-   var dx=(Math.random()-0.5)*8;
+   var dx=(Math.random()-0.5);
    
-   var dy=(Math.random()-0.5)*8;
+   var dy=(Math.random()-0.5);
    var radius=30;   
    
     circleArray.push(new Circle(x,y,dx,dy,radius));
